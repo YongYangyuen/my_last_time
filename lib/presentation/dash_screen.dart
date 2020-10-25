30b99c2fa578e5ace69fcdba13cd1f95d009a77d
Yong Yangyuen <kkingza@hotmail.com>
2020-10-25 21:25:14 +0700

    update bg color

--- lib/presentation/dash_screen.dart
+++ lib/presentation/dash_screen.dart
@@ -57,6 +57,7 @@ class _DashScreenState extends State<DashScreen> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
